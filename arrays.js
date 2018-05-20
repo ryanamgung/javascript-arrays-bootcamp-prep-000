0116ee52a74573d9275c2e6edbc7bc88797fe4b7
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-20 04:07:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,17 +9,19 @@ function addElementToBeginningOfArray(array, element)
 function destructivelyAddElementToBeginningOfArray(array, element)
 {
  array.unshift(element) 
+ return array
 }
 
 function addElementToEndOfArray(array, element)
 {
-  var newArray = [array, ...element]
+  var newArray = [...array, ...element]
   return newArray
 }
 
 function destructivelyAddElementToEndOfArray(array, element)
 {
  array.push(element) 
+ return array
 }
 
 function accessElementInArray(array, index)
